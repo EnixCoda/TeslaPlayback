d3b8d64a07800113fe00be4aa4df2e7f61675a9a
EnixCoda <enixcoda@gmail.com>
2024-03-21 16:07:53 +0800

    build: remove SASS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
-import "./index.scss";
 
 const appElement = document.getElementById("app");
 if (appElement) createRoot(appElement).render(<App />);
