59f1528f052fd09240c4510a45f3414d73dc7048
EnixCoda <enixcoda@gmail.com>
2024-04-20 21:16:48 +0800

    feat: handle error of loading demo videos

--- src/components/DemoVideoLoader.tsx
+++ src/components/DemoVideoLoader.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Text } from "@primer/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import back from "../../demo/RecentClips/2024-04-16_10-10-22-back.mp4";
 import front from "../../demo/RecentClips/2024-04-16_10-10-22-front.mp4";
 import leftRepeater from "../../demo/RecentClips/2024-04-16_10-10-22-left_repeater.mp4";
 import rightRepeater from "../../demo/RecentClips/2024-04-16_10-10-22-right_repeater.mp4";
+import { run } from "../utils/general";
 
-const loadDemoVideoFile = (importUrl: string, filePath = importUrl, onFileLoaded?: (file: File) => void): Promise<File> =>
+const loadDemoVideoFile = (importUrl: string, filePath = importUrl): Promise<File> =>
   fetch(importUrl)
     .then((r) => r.blob())
     .then((blob) => new File([blob], filePath.split("/").pop()!, { type: "video/mp4" }))
@@ -13,39 +14,52 @@ const loadDemoVideoFile = (importUrl: string, filePath = importUrl, onFileLoaded
       Object.defineProperty(file, "webkitRelativePath", {
         get: () => importUrl,
       });
-      onFileLoaded?.(file);
       return file;
     });
 
-const filesToLoad: string[] = [front, back, leftRepeater, rightRepeater];
+const demoFileUrls: string[] = [front, back, leftRepeater, rightRepeater];
 
 export function DemoVideoLoader({ setFileList }: { setFileList: (fileList: FileListLike) => void }) {
-  const [[loaded, totalToLoad], setLoadDemoFilesProgress] = useState<[number, number]>([0, 0]);
+  const [[filesLoaded, filesToLoad], setLoadDemoFilesProgress] = useState<[File[], string[]]>([[], []]);
+  const [error, setError] = useState<string | null>(null);
 
-  if (filesToLoad.length === 0) return null;
+  useEffect(() => {
+    let aborted = false;
+    const cancel = () => {
+      aborted = true;
+    };
+    run(async () => {
+      try {
+        setError(null);
+        const fileList = await Promise.all(
+          filesToLoad.map(async (path) => {
+            const file = await loadDemoVideoFile(path);
+            setLoadDemoFilesProgress(([loaded, totalToLoad]) => [loaded.concat(file), totalToLoad]);
+            return file;
+          })
+        );
+        if (aborted) return;
+        setFileList(fileList);
+      } catch (error) {
+        if (aborted) return;
+        console.error(error);
+        setError(`Failed loading demo videos.`);
+      }
+    });
+
+    return () => cancel();
+  }, [filesToLoad, setFileList]);
+
+  if (demoFileUrls.length === 0) return null;
   return (
     <Box>
-      <Button
-        onClick={async () => {
-          function loadDemoVideoFiles(onFileLoaded?: (file: File) => void) {
-            return Promise.all(filesToLoad.map((path) => loadDemoVideoFile(path, path, onFileLoaded)));
-          }
-
-          setLoadDemoFilesProgress([0, filesToLoad.length]);
-          setFileList(
-            await loadDemoVideoFiles(() => {
-              setLoadDemoFilesProgress(([loaded, totalToLoad]) => [loaded + 1, totalToLoad]);
-            })
-          );
-        }}
-      >
-        Try with demo files
-      </Button>
-      {totalToLoad > 0 && (
+      <Button onClick={() => setLoadDemoFilesProgress([[], demoFileUrls])}>Try with demo files</Button>
+      {filesToLoad.length > 0 && (
         <Text>
-          Loading demo video files: {loaded}/{totalToLoad}
+          Loading demo video files: {filesLoaded.length}/{filesToLoad.length}
         </Text>
       )}
+      {error !== null && <Text>Something went wrong: {error}</Text>}
     </Box>
   );
 }
