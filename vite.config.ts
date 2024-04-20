f995c1b5cf95fe9adab32e663578c74bd18f06bd
EnixCoda <enixcoda@gmail.com>
2024-04-20 15:00:57 +0800

    build: patch @ffmpeg/ffmpeg

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    {
+      name: "Patch @ffmpeg/ffmpeg package.json",
+      buildStart: () => {
+        const ffmpegPackageJsonPath = path.resolve(__dirname, "node_modules/@ffmpeg/ffmpeg/package.json");
+        const packageJson = JSON.parse(fs.readFileSync(ffmpegPackageJsonPath, "utf-8"));
+        if (packageJson.exports) {
+          Reflect.deleteProperty(packageJson, "exports");
+          fs.writeFileSync(ffmpegPackageJsonPath, JSON.stringify(packageJson, null, 2), "utf-8");
+        }
+      },
+    },
     react(),
     {
       name: "CrossOriginIsolationPlugin",
