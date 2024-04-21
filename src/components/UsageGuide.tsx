66e221531715ef751392e7d04493e9d9dbc93099
EnixCoda <enixcoda@gmail.com>
2024-04-21 13:46:44 +0800

    docs: update guide

--- src/components/UsageGuide.tsx
+++ src/components/UsageGuide.tsx
@@ -8,18 +8,25 @@ export function UsageGuide() {
       </Heading>
       <ol>
         <li>Disconnect the USB drive from your Tesla and connect it to your computer.</li>
-        <li>Initiate the process by clicking the "Load files" button.</li>
+        <li>Initiate the process by clicking the "Load DashCam Files" button.</li>
         <li>
-          Navigate to and select the USB drive or its root folder, which contains <em>TeslaCam</em>, <em>RecentClips</em>, <em>SavedClips</em>, or{" "}
+          Navigate to and select the USB drive or its root folder <em>TeslaCam</em>, which contains <em>RecentClips</em>, <em>SavedClips</em>, or{" "}
           <em>SentryClips</em>.
-          <p>
-            Important: If your browser prompts a warning about file uploads, don't worry, no files will be uploaded. For added security, feel free to
-            disconnect from internet before you proceed.
-          </p>
         </li>
         <li>Upon selecting the correct folder, your Tesla's video files will automatically begin playback.</li>
       </ol>
-      <p>Note: This web app is for 🖥️ 💻 computer usages (Windows/macOS/Linux). It might not work on phone or tablet.</p>
+      <section>
+        <Heading as="h3" sx={{ fontSize: 2 }}>
+          Note
+        </Heading>
+        <ul>
+          <li>
+            No files will be uploaded. If your browser prompts a warning about file uploads, please ignore it. For added security, feel free to
+            disconnect from internet before you proceed.
+          </li>
+          <li>This web app is for 🖥️ 💻 computer usages (Windows/macOS/Linux). It might not work on phone or tablet.</li>
+        </ul>
+      </section>
     </section>
   );
 }
